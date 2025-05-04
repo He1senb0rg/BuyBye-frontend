5ee3ba9c63b8d4e35be9d3ccabfe2d116b2881e8
Guilherme Sardinha <83502530+william-217@users.noreply.github.com>
2025-05-04 15:51:34 +0100

    Version 1.4.2, hotfix

--- app/components/cart/CartPage.jsx
+++ app/components/cart/CartPage.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const CartPage = ({ children }) => {
   return (
-    <main className="container-fluid align-content-lg-stretch">
+    <main className="container-fluid">
       <div className="row">
         {/* Make left column take more space on large screens */}
-        <div className="col-12 col-lg-9 mb-4">
+        <div className="col-md-8 offset-md-1">
           {children[0] /* CartHeader */}
           {children[1] /* CartItemsList */}
         </div>
 
         {/* CartSummary stays on the right, smaller */}
-        <div className="col-12 col-lg-3">
+        <div className="col-md-4">
           {children[2] /* CartSummary */}
         </div>
       </div>
