81038d8d44fdd7c765156de6108a004b66b8e12f
LilDAB <pipo.brites2005@gmail.com>
2025-05-19 15:00:00 +0100

    Update ShopEditor.jsx

--- app/pages/ShopEditor.jsx
+++ app/pages/ShopEditor.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import AccountNavigation from '../components/accountDetailsComponents/AccountNavigation';
-import ProfilePictureCard from '../components/accountDetailsComponents/ProfilePictureCard';
-import AccountDetailsCard from '../components/accountDetailsComponents/AccountDetailsCard';
+import ShopPictureCard from '../components/shopEditorComponents/ShopPictureCard';
+import ShopDetailsCard from '../components/shopEditorComponents/ShopDetailsCard';
 
 const ShopEditor = () => {
   return (
     <main>
       <div className="container-xl px-4 mt-4">
-        <AccountNavigation activePage="profile" />
+        <div className="row">
+          <div className="col">
+            <h1 className="mt-4">Editar Loja</h1>
+          </div>
+        </div>
         <hr className="mt-0 mb-3" />
         <div className="row">
           <div className="col-md-6 mb-3">
-            <ProfilePictureCard />
+            <ShopPictureCard />
           </div>
           <div className="col-md-6 mb-3">
-            <AccountDetailsCard />
+            <ShopDetailsCard />
           </div>
         </div>
       </div>
