4ac175674669cb6656fc233d14d3747924395f03
LilDAB <pipo.brites2005@gmail.com>
2025-05-19 13:05:57 +0100

    Create ShopEditor.jsx

---
+++ app/pages/ShopEditor.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import AccountNavigation from '../components/accountDetailsComponents/AccountNavigation';
+import ProfilePictureCard from '../components/accountDetailsComponents/ProfilePictureCard';
+import AccountDetailsCard from '../components/accountDetailsComponents/AccountDetailsCard';
+
+const ShopEditor = () => {
+  return (
+    <main>
+      <div className="container-xl px-4 mt-4">
+        <AccountNavigation activePage="profile" />
+        <hr className="mt-0 mb-3" />
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <ProfilePictureCard />
+          </div>
+          <div className="col-md-6 mb-3">
+            <AccountDetailsCard />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ShopEditor;
